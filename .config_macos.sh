2e3cf95ab39e2bcc12360bf3621c94b0719bf1d5
inigoliz <inilariz@gmail.com>
2022-07-14 20:10:09 +0200

    add macos config

---
+++ .config_macos.sh
@@ -0,0 +1,4 @@
+defaults write -g com.apple.trackpad.scaling 2 #set trackpad speed
+defaults write com.apple.dock autohide-delay -float 0 #delay to show dock
+defaults write com.apple.dock autohide-time-modifier -float 0.5 #duration dock animation
+defaults write com.apple.dock showhidden -bool true #traslucid hidden apps (cmd+H)
