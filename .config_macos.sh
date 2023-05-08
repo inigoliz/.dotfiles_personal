1ff7ff1d5a526294a172e9437590164a7d77fdc8
Ignigo <55053999+inigoliz@users.noreply.github.com>
2023-05-08 17:12:23 +0200

    Update .config_macos.sh

--- .config_macos.sh
+++ .config_macos.sh
@@ -1,7 +1,10 @@
-defaults write -g com.apple.trackpad.scaling -float 2.5 #set trackpad speed
+defaults write com.apple.dock magnification -bool true # magnification
+defaults write com.apple.dock tilesize -int 50 # normal icon size
+defaults write com.apple.dock largesize -int 90 # magnified icon size
+defaults write com.apple.dock autohide-delay -int 0 # delay to show dock
+defaults write com.apple.dock autohide-time-modifier -float 0.5 # duration dock animation
+defaults write com.apple.dock showhidden -bool true # traslucid hidden apps (cmd+H)
 
-defaults write com.apple.dock autohide-delay -int 0 #delay to show dock
-defaults write com.apple.dock largesize -int 95 #magnification
-defaults write com.apple.dock autohide-time-modifier -float 0.5 #duration dock animation
-defaults write com.apple.dock showhidden -bool true #traslucid hidden apps (cmd+H)
+defaults write -g com.apple.trackpad.scaling -float 2.5 #set trackpad speed. Needs restart
+
 killall Dock #restart dock
