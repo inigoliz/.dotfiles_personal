903c04704055c804e337794b832f769412c92a4c
Iñigo Lara <166121875+ignigoliz@users.noreply.github.com>
2024-05-22 07:39:15 +0200

    Update .config_macos.sh

--- .config_macos.sh
+++ .config_macos.sh
@@ -1,12 +1,13 @@
-defaults write com.apple.dock magnification -bool true;           # magnification
-defaults write com.apple.dock tilesize -int 50;                   # normal icon size
-defaults write com.apple.dock largesize -int 90;                  # magnified icon size
+defaults write com.apple.dock autohide -bool true                 # autohide dock
+defaults write com.apple.dock magnification -bool true;           # dock hover magnification
+defaults write com.apple.dock tilesize -int 74;                   # dock normal icon size
+defaults write com.apple.dock largesize -int 110;                  # dock magnified icon size
 defaults write com.apple.dock autohide-delay -int 0;              # delay to show dock
 defaults write com.apple.dock autohide-time-modifier -float 0.5;  # duration dock animation
 defaults write com.apple.dock showhidden -bool true;              # traslucid hidden apps (cmd+H)
 defaults write com.apple.dock show-recents -bool true;            # show recents
 defaults write com.apple.dock show-recent-count -int 6;           # number of recents to show
-defaults write com.apple.TextEdit NSShowAppCentricOpenPanelInsteadOfUntitledFile -bool false # open textedit directly
-
 
 killall Dock #restart dock
+
+defaults write com.apple.TextEdit NSShowAppCentricOpenPanelInsteadOfUntitledFile -bool false # open textedit directly
